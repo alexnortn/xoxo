05404b1a92d74426fdf8438b180ceb249e1dc8de
Alex Norton <alexnortn@gmail.com>
2017-12-30 15:09:31 -0500

    first commit, test React APP for xoxo

---
+++ src/Components/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        XOXO loves you.
+      </div>
+    );
+  }
+}
+
+export default Projects;
